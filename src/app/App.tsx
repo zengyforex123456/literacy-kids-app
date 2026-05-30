459689fe701ee2595b70e95c0038df7121aea535
Developer <dev@literacy.app>
2026-05-30 10:49:29 +0800

    fix: BrowserRouter→HashRouter 修复APK白屏

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import { useEffect } from 'react'
 import { HomeScreen } from '../screens/home/HomeScreen'
 import { TreasureHuntGame } from '../games/treasure-hunt/TreasureHuntGame'
@@ -28,7 +28,7 @@ export default function App() {
   useEffect(() => { seedWords().catch(() => {}) }, [])
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <EyeCareOverlay />
       <Routes>
         <Route path="/" element={<HomeScreen />} />
@@ -42,6 +42,6 @@ export default function App() {
         <Route path="/parent" element={<ParentScreen />} />
         <Route path="/report" element={<ReportScreen />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
